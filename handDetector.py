d1ab2cdb3864800d8e5196c6973bb20ad1519f15
YorneV <100277029+YorneV@users.noreply.github.com>
2022-06-11 13:49:45 +0200

    Merge
    lmao

--- handDetector.py
+++ handDetector.py
@@ -1,4 +1,3 @@
-import numpy
 import mediapipe as mp
 import cv2
 import mouse
@@ -122,7 +121,107 @@ class handDetector():
         self.prev_middel_x, self.prev_middel_y = self.middel_x, self.middel_y
 
 
+class handDetectorGrid():
+    def __init__(self, mode=False, maxHands = 2, confidence= 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.confidence = confidence
 
+        self.mp_hands = mp.solutions.hands
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_styles = mp.solutions.drawing_styles
+        self.hands = self.mp_hands.Hands(model_complexity=0, max_num_hands=self.maxHands, static_image_mode=self.mode, min_detection_confidence=self.confidence)
+
+        self.cap = cv2.VideoCapture(0)
+        self.index_x = None
+        self.index_y = None
+        self.thumb_x = None
+        self.thumb_y = None
+        self.wrist_x = None
+        self.wrist_y = None
+        self.distance = None
+
+        self.prev_wrist_x = None
+        self.prev_wrist_y = None
+        try:
+            self.width, self.height = screen.size()
+        except:
+            pass
+        try:
+            self.width, self.height = GetSystemMetrics(0),GetSystemMetrics(1)
+        except:
+            pass
+        self.clicked = False
+
+    def webCam(self):
+        while self.cap.isOpened():
+            succes, image = self.cap.read()
+            if not succes:
+                print('Broken frame')
+                #broken frames need to be skipped
+                continue
+            image.flags.writeable = False  # to make it quicker
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imshow("VideoStream", cv2.flip(image,1))
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break #press q to end it
+            self.cap.release
+
+    def fingerControl(self):
+        while self.cap.isOpened():
+            succes, image = self.cap.read()
+            if not succes:
+                continue #skip broken frames
+            image.flags.writeable = False  # to make it quicker
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = cv2.flip(image, 1)
+            results = self.hands.process(image)  # process de frame
+
+            if results.multi_hand_landmarks:
+                self.index_x = int(results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].x*image.shape[1])
+                self.index_y = int(results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y*image.shape[0])
+
+                self.thumb_x = int(results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.THUMB_TIP].x*image.shape[1])
+                self.thumb_y = int(results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.THUMB_TIP].y*image.shape[0])
+
+                self.wrist_x = results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.WRIST].x
+                self.wrist_y = results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.WRIST].y
+
+                self.distance = ((self.index_x-self.thumb_x)**2 + (self.index_y-self.thumb_y)**2 )**0.5
+
+                self.mouseControl()
+                cv2.imshow("VideoStream", cv2.flip(image,1))
+                if cv2.waitKey(5) & 0xFF == ord('q'):
+                    break  # press q to end it
+                self.cap.release
+
+    def mouseControl(self):
+        threshold = 1/100 #it's in percentages
+        thresholdclick = 20
+        gridsize = 120
+
+        try:
+            if abs(self.prev_wrist_x - self.wrist_x) > threshold or abs(self.prev_wrist_y - self.wrist_y) > threshold: #if distance is less then threshold then probably noise
+                self.wrist_x = min(max(self.wrist_x*(1/0.8)-(0.1/0.8), 0), 1)
+                self.wrist_y = min(max(self.wrist_y*(1/0.8)-(0.1/0.8), 0), 1)
+                x = self.wrist_x*self.width
+                y = self.wrist_y*self.height
+                for i in range(0,gridsize):
+                    if round(self.width/gridsize)*(i+1) > x > round(self.width/gridsize)*(i):
+                        x = round(self.width/gridsize)*(i+1) 
+                    if round(self.height/gridsize)*(i+1) > y > round(self.height/gridsize)*(i):
+                        y = round(self.height/gridsize)*(i+1)
+                        break
+                mouse.move(x, y)
+            if self.distance < thresholdclick and not self.clicked:
+                mouse.click('left')
+                print("geklikt")
+                self.clicked = True
+            if self.distance > (thresholdclick + 2) and self.clicked:
+                self.clicked = False   
+        except:
+            pass
+        self.prev_wrist_x, self.prev_wrist_y = self.wrist_x, self.wrist_y
 
 if __name__ == '__main__':
     handDetector(maxHands=2, mode=False, confidence=0.8).fingerControl()
